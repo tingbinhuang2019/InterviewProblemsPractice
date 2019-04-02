bc1e0705b255de4a9cdf21c53b0abcfd78fcee73
tingbinhuang2019 <thuang18@usfca.edu>
2019-04-02 08:10:53 -0700

    Implemented merge sort in es6

--- practiceProblems/merge.js
+++ practiceProblems/merge.js
@@ -1,42 +1,27 @@
 
 let merge = (arr1, arr2) => {
-	let newArray = []; 
-	let i = 0;
-	let j = 0; 
+	let i = 0, j = 0, newArray = []; 
 
 	while(i < arr1.length && j < arr2.length) {
 		if(arr1[i] < arr2[j]) {
-			newArray.push(arr1[i]);
-			i++;
+			newArray.push(arr1[i++])
 		} else {
-			newArray.push(arr2[j]);
-			j++;
+			newArray.push(arr2[j++]);
 		}
 	}
-
-	while( i < arr1.length) {
-		newArray.push(arr1[i]);
-		i++;
+	while(i < arr1.length) {
+		newArray.push(arr1[i++]);
 	}
-
-	while( j < arr2.length) {
-		newArray.push(arr2[j]);
-		j++;
+	while(j < arr2.length) {
+		newArray.push(arr2[j++]);
 	}
 	return newArray;
 }
 
 let mergeSort = (arr) => {
-	if(arr.length <= 1) return arr; 
-	let midPoint = Math.floor(arr.length / 2);
-	let arr1 = arr.slice(0, midPoint);
-	let arr2 = arr.slice(midPoint);
-	let left = mergeSort(arr1);
-	let right = mergeSort(arr2);
-
-	return merge(left,right);
+	if(arr.length <= 1) return arr;
+	let mid = Math.floor(arr.length / 2);
+	let arr1 = mergeSort(arr.slice(0, mid));
+	let arr2 = mergeSort(arr.slice(mid));
+	return merge(arr1,arr2);
 }
-
-
-
-
