6b5efb41e9df40749a856cc04e020eaf3b28e5bc
tingbinhuang2019 <thuang18@usfca.edu>
2019-04-05 11:27:54 -0700

    Implemented Graph class in es6

--- practiceProblems/Graph.js
+++ practiceProblems/Graph.js
@@ -27,4 +27,42 @@ class Graph {
     }
     delete this.ajacencyList[v];
   }
+
+  // dfs recursive version
+  dfsRecursive(start) {
+    let data = [];
+    let visited = {};
+    let dfs = (start) => {
+      data.push(start);
+      visited[start] = true;
+      for (let i = 0; i < this.ajacencyList[start].length; i++) {
+        let vertex = this.ajacencyList[start][i];
+        if (!visited[vertex]) {
+          dfs(vertex);
+        }
+      }
+    }
+    dfs(start);
+    return data;
+  }
+
+  // dfs iterative version
+  dfsIterative(start) {
+    let data = [];
+    let visited = {};
+    let stack = [];
+    stack.push(start);
+    while (stack.length) {
+      let item = stack.pop();
+      if (!visited[item]) {
+        visited[item] = true;
+        data.push(item);
+        for (let i = 0; i < this.ajacencyList[item].length; i++) {
+          let vertex = this.ajacencyList[item][i];
+          stack.push(vertex);
+        }
+      }
+    }
+    return data;
+  }
 }
