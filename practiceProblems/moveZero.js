f749a8120e3d5244426e9a3090d2647eef1d5c3c
tingbinhuang2019 <thuang18@usfca.edu>
2019-04-12 16:54:27 -0700

    Implemented LeetCode #283

--- practiceProblems/moveZero.js
+++ practiceProblems/moveZero.js
@@ -1,15 +1,16 @@
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
+// function to swap elements
 let swap = (nums, i, j) => {
   let temp = nums[i];
   nums[i] = nums[j];
   nums[j] = temp;
 }
 
+// main function
 var moveZeroes = function (nums) {
   let slow = 0, fast = 0;
-
   while (fast < nums.length) {
     if (nums[fast] !== 0) {
       swap(nums, slow, fast);
