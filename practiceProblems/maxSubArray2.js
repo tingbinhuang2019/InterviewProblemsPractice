ac5ead595f749b7c9aea03b3a75983a4ec641d59
tingbinhuang2019 <thuang18@usfca.edu>
2019-04-30 13:19:19 -0700

    Implemented LeetCode #53

--- practiceProblems/maxSubArray2.js
+++ practiceProblems/maxSubArray2.js
@@ -1,15 +1,18 @@
 
 // Find contiguous subarray that has the largest sum from the input array.
 
-let findMax = (arr) => {
-  let maxVal = 0;
-  let max_array = [arr[0]];
+let findMax = (nums) => {
 
-  for (let i = 1; i < arr.length; i++) {
-    max_array[i] = Math.max(0, arr[i] + max_array[i - 1]);
-    maxVal = Math.max(maxVal, max_array[i]);
+  let tempMax = nums[0];
+  let maxVal = tempMax;
+
+  for (let i = 1; i < nums.length; i++) {
+    tempMax = Math.max(tempMax, tempMax + nums[i]);
+    maxVal = maxVal < tempMax ? tempMax : maxVal;
   }
   return maxVal;
 }
 
-console.log(findMax([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(findMax([-2, 1]));
+
+
