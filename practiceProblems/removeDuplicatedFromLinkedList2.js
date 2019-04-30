e88b03a83af935d5fca973bdd94294bcfb780f1b
tingbinhuang2019 <thuang18@usfca.edu>
2019-04-30 15:45:12 -0700

    Implemented LeetCode #83

--- practiceProblems/removeDuplicatedFromLinkedList2.js
+++ practiceProblems/removeDuplicatedFromLinkedList2.js
@@ -21,3 +21,21 @@ var deleteDuplicates = function (head) {
   }
   return dummyHead.next;
 }
+
+let printNode = (node) => {
+  let cur = node;
+  let result = "";
+  while (cur.next !== null) {
+    result += cur.val.toString() + " -> ";
+    cur = cur.next;
+  }
+  result += cur.val.toString();
+  console.log(result);
+}
+
+let a = new ListNode(1);
+a.next = new ListNode(1);
+a.next.next = new ListNode(2);
+a.next.next.next = new ListNode(2);
+a.next.next.next.next = new ListNode(4);
+printNode(deleteDuplicates(a)); // 1 -> 2 -> 4 
