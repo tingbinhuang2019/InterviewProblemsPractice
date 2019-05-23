f092b2219abccb0b3b0e6202013ceb00f84397df
tingbinhuang2019 <thuang18@usfca.edu>
2019-05-22 18:37:39 -0700

    Implemented LeetCode #114

---
+++ practiceProblems/flatten.js
@@ -0,0 +1,31 @@
+var TreeNode = function (val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+let treeToLinkedList = (root) => {
+  let pre = null;
+
+  let flatten = (root) => {
+    if (root === null) {
+      return;
+    }
+
+    flatten(root.right);
+    flatten(root.left);
+    root.right = pre;
+    root.left = null;
+    pre = root;
+  }
+  flatten(root);
+  return root;
+}
+
+let tree = new TreeNode(5);
+tree.left = new TreeNode(3);
+tree.right = new TreeNode(6);
+tree.left.left = new TreeNode(2);
+tree.left.right = new TreeNode(4);
+tree.right.right = new TreeNode(7);
+
+console.log(treeToLinkedList(tree));
