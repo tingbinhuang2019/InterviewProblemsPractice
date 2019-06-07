b5df74f531f336b5535e9b64cce93466eb21fe0b
tingbinhuang2019 <thuang18@usfca.edu>
2019-06-06 18:21:20 -0700

    Implemented LeetCode # 33

--- practiceProblems/searchInRotatedSortedArray.js
+++ practiceProblems/searchInRotatedSortedArray.js
@@ -2,44 +2,43 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
- * # 33 
+ * # 33
  */
 var search = function (nums, target) {
+  if (nums === null) {
+    return -1;
+  }
 
-  let min, mid;
-  let l = 0, r = nums.length - 1;
+  let l = 0, r = nums.length - 1, mid;
 
-  while (l < r) {
+  while (l <= r) {
     mid = l + Math.floor((r - l) / 2);
-    if (nums[mid] > nums[r]) {
-      l = mid + 1;
-    } else {
-      r = mid;
-    }
-  }
-  min = l;
-  let end = nums.length - 1;
-  if (target > nums[end]) {
-    return binarySearch(nums, 0, min - 1, target);
-  } else {
-    return binarySearch(nums, min, end, target);
-  }
-};
 
-let binarySearch = (arr, l, r, target) => {
-  if (l <= r) {
-    let m = l + Math.floor((r - l) / 2);
-    if (arr[m] === target) {
-      return m;
-    } else if (arr[m] > target) {
-      return binarySearch(arr, l, m - 1, target);
-    } else {
-      return binarySearch(arr, m + 1, r, target);
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] > nums[r]) {
+      if (target >= nums[l] && target < nums[mid]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+
+    else {
+      if (nums[mid] < target && target <= nums[r]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
   }
   return -1;
-}
+};
+//         0, 1, 2, 3, 4, 5, 6, 7, 8
+let arr = [4, 5, 6, 7, 8, 0, 1, 2, 3];
 
-let arr = [4, 5, 6, 7, 0, 1, 2];
 
-console.log(search(arr, 4));
+
+console.log(search(arr, 12));
