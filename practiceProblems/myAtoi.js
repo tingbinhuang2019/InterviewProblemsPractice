ee7f61dea8d8a15a3d3cc93d12933157c815fa02
tingbinhuang2019 <thuang18@usfca.edu>
2019-06-17 14:54:08 -0700

    Implemented LeetCode # 8

--- practiceProblems/myAtoi.js
+++ practiceProblems/myAtoi.js
@@ -5,18 +5,19 @@
  */
 
 let isNumber = (str) => {
+
   return !isNaN(str);
 }
-
 var myAtoi = function (str) {
-
   if (str === null || !str) {
     return 0;
   }
-  let sign = 1;
+
   let start = 0;
-  let res = 0;
+  let sign = 1;
   let i = 0;
+  let res = 0;
+  str = str.trim();
 
   if (str[i] === '+') {
     sign = 1;
@@ -28,19 +29,32 @@ var myAtoi = function (str) {
   i = start;
 
   while (i < str.length) {
-    if (!isNumber(str[i])) {
+
+    if (str[i] !== ' ') {
+      if (i === 1) {
+
+        if (str[i] === '0' && str[i - 1] === '+' && str[i + 1] === ' ') {
+          return 0;
+        } else if (str[i] === '0' && str[i - 1] === '-' && str[i + 1] === ' ') {
+          return 0;
+        }
+      }
+      if (!isNumber(str[i])) {
+        return res * sign;
+      }
+      res *= 10;
+      res += str[i] - '0';
+
+      if (sign === 1 && res >= 2147483648) {
+        return sign * (2147483648 - 1);
+      } else if (sign === -1 && res > 2147483648) {
+        return sign * 2147483648;
+      }
+    } else {
       return res * sign;
     }
-    res *= 10;
-    res += str[i] - '0';
-    if (res > Number.MAX_SAFE_INTEGER) {
-      return sign * Number.MAX_SAFE_INTEGER;
-    }
+
     i++;
   }
   return res * sign;
 };
-
-console.log(myAtoi('+11235653634512ddd')); // 11235653634512 
-console.log(myAtoi('-11235653634512ddd')); // - 11235653634512ddd
-
